981ef654fd5bea56df3130e50b2fc9252817cc78
DESKTOP-EH772EC\Yielonek <karol26688@gmail.com>
2022-10-22 17:15:37 +0200

    Improving Image Upload

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-UserImagePicker(this.imagePickFn);
+  UserImagePicker(this.imagePickFn);
 
-final void Function(File pickedImage) imagePickFn;
+  final void Function(File pickedImage) imagePickFn;
 
   @override
   State<UserImagePicker> createState() => _UserImagePickerState();
@@ -16,8 +16,11 @@ class _UserImagePickerState extends State<UserImagePicker> {
   File _pickedImage;
 
   void _pickImage() async {
-    final pickedImageFile =
-        await ImagePicker.pickImage(source: ImageSource.camera);
+    final pickedImageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
     setState(() {
       _pickedImage = pickedImageFile;
     });
